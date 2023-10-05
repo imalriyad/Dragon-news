b5bbd1287ed4e587e5be5e54fefe53edd52621bf
imalriyad <imalriyad@gmail.com>
2023-10-05 18:04:36 +0600

    fixed

--- src/SharedComponent/LeftNav.jsx
+++ src/SharedComponent/LeftNav.jsx
@@ -9,38 +9,48 @@ import { DataContext } from "../Layout/Layout";
 import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
 import toast, { Toaster } from "react-hot-toast";
 const LeftNav = () => {
-  const { githubSignIn, googleSignIn } = useContext(DataContext);
+  const { user, githubSignIn, googleSignIn } = useContext(DataContext);
 
-
-  const googleSignInHandler = ()=>{
-      const gooleAuthProvider= new GoogleAuthProvider()
-      googleSignIn(gooleAuthProvider)
+  const googleSignInHandler = () => {
+    const gooleAuthProvider = new GoogleAuthProvider();
+    googleSignIn(gooleAuthProvider)
       .then(() => {
-        toast.success('Successfully Registerd!')
-   
-       })
-       .catch(error=> toast.error(`${error.message.slice(22,).replace(')','')}`))
-  }
-  const githubSignInHandler = ()=>{
-     const githubAuthProvider = new GithubAuthProvider()
-     githubSignIn(githubAuthProvider)
-     .then(() => {
-      toast.success('Successfully Registerd!')
-
-     })
-     .catch(error=> toast.error(`${error.message.slice(22,).replace(')','')}`))
-  }
+        toast.success("Successfully Registerd!");
+      })
+      .catch((error) =>
+        toast.error(`${error.message.slice(22).replace(")", "")}`)
+      );
+  };
+  const githubSignInHandler = () => {
+    const githubAuthProvider = new GithubAuthProvider();
+    githubSignIn(githubAuthProvider)
+      .then(() => {
+        toast.success("Successfully Registerd!");
+      })
+      .catch((error) =>
+        toast.error(`${error.message.slice(22).replace(")", "")}`)
+      );
+  };
   return (
     <div>
-      <h1 className="font-semibold text-2xl">Login With</h1>
-      <div className="flex-col flex">
-        <button onClick={googleSignInHandler} className="btn btn-outline my-3">
-          <FcGoogle className="text-xl"></FcGoogle>Login With Google
-        </button>
-        <button onClick={githubSignInHandler} className="btn btn-outline">
-          <FaGithub className="text-xl"></FaGithub>Login With Github
-        </button>
-      </div>
+     
+      {user ? (
+        ""
+      ) : (
+        
+        <div className="flex-col flex">
+           <h1 className="font-semibold text-2xl">Login With</h1>
+          <button
+            onClick={googleSignInHandler}
+            className="btn btn-outline my-3"
+          >
+            <FcGoogle className="text-xl"></FcGoogle>Login With Google
+          </button>
+          <button onClick={githubSignInHandler} className="btn btn-outline">
+            <FaGithub className="text-xl"></FaGithub>Login With Github
+          </button>
+        </div>
+      )}
       <div className="flex flex-col mb-5">
         <h1 className="font-semibold text-2xl mt-8">Find Us On</h1>
         <Link to={"https://www.facebook.com/imalriyad"}>
@@ -62,11 +72,23 @@ const LeftNav = () => {
       </div>
       <div className="bg-[#F3F3F3] p-6 rounded space-y-7">
         <h1 className="font-semibold mb-5 text-2xl">Q-Zone</h1>
-        <img src="https://i.ibb.co/BNK2nVH/qZone1.png" className="mx-auto cursor-pointer" alt="" />
-        <img src="https://i.ibb.co/MnsFJFd/qZone2.png" className="mx-auto cursor-pointer" alt="" />
-        <img src="https://i.ibb.co/B6LFBjh/qZone3.png" className="mx-auto cursor-pointer" alt="" />
+        <img
+          src="https://i.ibb.co/BNK2nVH/qZone1.png"
+          className="mx-auto cursor-pointer"
+          alt=""
+        />
+        <img
+          src="https://i.ibb.co/MnsFJFd/qZone2.png"
+          className="mx-auto cursor-pointer"
+          alt=""
+        />
+        <img
+          src="https://i.ibb.co/B6LFBjh/qZone3.png"
+          className="mx-auto cursor-pointer"
+          alt=""
+        />
       </div>
-      <Toaster/>
+      <Toaster />
     </div>
   );
 };
